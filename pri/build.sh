67df63d8b989971e7659ccb27e305ffb9df64424
PuPuppru <zhengruru123@gmail.com>
2020-07-22 00:30:48 +0800

    Revert "add sdfat configs"
    This reverts commit dfdcd349ece86e0b1638ac0f3d6ca21e5d76cc50.

--- pri/build.sh
+++ pri/build.sh
@@ -2,7 +2,7 @@
 rm -rf out
 mkdir -p out
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=/media/pupuppru/puo/android_kernel_lge_sm8150/pri/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+export CROSS_COMPILE=../pri/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
 make O=./out ARCH=arm64 vendor/flashlmdd_lao_com-perf_defconfig
 make O=./out ARCH=arm64 REAL_CC=/media/pupuppru/puo/android_kernel_lge_sm8150/pri/tools/clang/host/linux-x86/clang-r349610/bin/clang -j12
 cp -f ./out/arch/arm64/boot/Image.gz-dtb ./pri/tools/boot/Image.gz-dtb
